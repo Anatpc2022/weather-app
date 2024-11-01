614246d8f8791663337e356d3ef1684de54e13f7
Anatpc2022 <ana.tpc2022@gmail.com>
2024-11-01 18:52:18 -0300

    Implementando formulário de pesquisa

--- src/app/modules/wheater/page/weather-home/weather-home.component.ts
+++ src/app/modules/wheater/page/weather-home/weather-home.component.ts
@@ -1,28 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
+import { Subject, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: [],
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
   initialCityName = 'São Paulo';
   weatherDatas!: WeatherDatas;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService: WeatherService) {}
+
   ngOnInit(): void {
     this.getWeatherDatas(this.initialCityName);
   }
 
   getWeatherDatas(cityName: string): void {
-    this.weatherService.getWeatherDatas(cityName).subscribe({
-      next: (response) => {
-        response && (this.weatherDatas = response);
-        console.log(this.weatherDatas);
-      },
-      error: (error) => console.log(error),
-    });
+    this.weatherService
+      .getWeatherDatas(cityName)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          response && (this.weatherDatas = response);
+          console.log(this.weatherDatas);
+        },
+        error: (error) => console.log(error),
+      });
+  }
+
+  onSubmit(): void {
+    this.getWeatherDatas(this.initialCityName);
+    console.log('CHAMOU A FUNÇÃO');
+    this.initialCityName = '';
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
